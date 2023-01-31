54d9fca1dd4c32de9fceb83d5e2f5117c244ab92
omer_in <107422716+ome-r@users.noreply.github.com>
2023-01-31 19:34:31 +0900

    feat: page

--- project/src/components/Paging.jsx
+++ project/src/components/Paging.jsx
@@ -2,23 +2,27 @@ import React, { useState } from "react";
 import "../css/paging.css";
 import Pagination from "react-js-pagination";
 
+// const articles = { articles };
 const Paging = ({ page, count, setPage }) => {
   return (
-    <Pagination
-      //현재 보고 있는 페이지
-      activePage={page}
-      //한 페이지에 출력할 아이템수
-      itemsCountPerPage={1}
-      //총 아이템 수
-      totalItemsCount={count}
-      //표시할 페이지 갯수
-      pageRangeDisplayed={5}
-      // 넘기는 버튼 추가
-      prevPageText={"‹"}
-      nextPageText={"›"}
-      //함수
-      onChange={setPage}
-    />
+    <>
+      {console.log(count)};
+      <Pagination
+        //현재 보고 있는 페이지
+        activePage={page}
+        //한 페이지에 출력할 아이템수
+        itemsCountPerPage={1}
+        //총 아이템 수
+        totalItemsCount={count}
+        //표시할 페이지 갯수
+        pageRangeDisplayed={5}
+        // 넘기는 버튼 추가
+        prevPageText={"‹"}
+        nextPageText={"›"}
+        //함수
+        onChange={setPage}
+      />
+    </>
   );
 };
 
